98495f9bd54c37a1db7af157cc498c0885f5dfc2
hadi <hadi@depository.id>
2023-03-19 22:46:24 +0700

    create blank document

--- quickstart.go
+++ quickstart.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"quickstart/core"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -73,24 +74,25 @@ func main() {
 		log.Fatalf("Unable to read client secret file: %v", err)
 	}
 
+	fmt.Println(b)
+
 	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/documents.readonly")
+	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/documents")
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
+	fmt.Println(config)
 	client := getClient(config)
+	fmt.Println(client)
 
 	srv, err := docs.NewService(ctx, option.WithHTTPClient(client))
 	if err != nil {
 		log.Fatalf("Unable to retrieve Docs client: %v", err)
 	}
 
-	// Prints the title of the requested doc:
-	// https://docs.google.com/document/d/195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE/edit
-	docId := "195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE"
-	doc, err := srv.Documents.Get(docId).Do()
+	d, err := core.CreateBlankDoc(srv, "second-test")
 	if err != nil {
-		log.Fatalf("Unable to retrieve data from document: %v", err)
+		fmt.Println(err)
 	}
-	fmt.Printf("The title of the doc is: %s\n", doc.Title)
+	fmt.Println("Succes create new blank document: " + d.Title)
 }
